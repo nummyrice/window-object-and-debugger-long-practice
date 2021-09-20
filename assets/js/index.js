767d7a4ceffadf50e60f47e059a91412a18c93ab
nummyrice <nicholasroyrice@gmail.com>
2021-09-20 16:17:04 -0700

    phase 2 complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,13 @@
-changeTitle();
-changeHeader();
-changeAboutMe();
-console.log(findElementById("header")); // <div id="header">
-console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
-console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
-console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
-console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+import { changeTitle, changeHeader, changeAboutMe } from './manipulate.js';
+import { findElementById, findFirstElementOfClass, findElementsOfTag, findElementsOfClass, findFirstElementOfTag } from './search.js';
+
+window.onload = () => {
+    changeTitle();
+    changeHeader();
+    changeAboutMe();
+    console.log(findElementById("header")); // <div id="header">
+    console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
+    console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
+    console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
+    console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+};
--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,16 +1,16 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
     // Your code here
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
     // Your code here
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
 
